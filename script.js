c5e8462b02d281b1719c82e817c3a1a3ce3db2c6
psnjit <prasenjitsaha49@gmail.com>
2024-04-13 13:08:44 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,15 @@
+// const result=document.getElementById("result");
+// let displayText='';
+
+// function clickHandler(Event){
+//     console.log("clicked");
+//     console.log(Event.target.textContent);
+//     const value=Event.target.textContent;
+//     displayText=displayText?displayText:''+value;
+//     if(value==='AC')
+//         displayText='';
+//     if(value==='DE' && displayText.length>0)
+//         displayText=displayText.slice(0, -1);
+
+//     result.textContent=displayText;
+// }
